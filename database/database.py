60b6c35daa502da86fbfda0d739fbc92cc7d753e
Otim64 <brainyrene12@gmail.com>
2026-07-15 23:10:54 +0300

    Print database path

--- database/database.py
+++ database/database.py
@@ -2,10 +2,13 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 PROJECT_DIR = os.path.dirname(BASE_DIR)
-
 DB_PATH = os.path.join(PROJECT_DIR, "students.db")
+
+print("=" * 60)
+print("DATABASE PATH:", DB_PATH)
+print("DATABASE EXISTS:", os.path.exists(DB_PATH))
+print("=" * 60)
 
 
 def get_connection():
